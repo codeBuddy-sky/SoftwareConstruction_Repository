2a03f71c52f436875f65089eda606e9132121686
Akash Gupta <32446244+akashgupta2612@users.noreply.github.com>
2019-09-30 16:42:59 +0530

    Update find_freq_byRec.c

--- Codes/find_freq_byRec.c
+++ Codes/find_freq_byRec.c
@@ -1,38 +1,42 @@
 #include<stdio.h>
 #include<string.h>
+#include<assert.h>
 
-int s,k,j=0,i=0,x=0,a[100],occ=0;
+int size=0,k,j=0,i=0,x=0,a[100],occ=0;
 
-int rec(int a[],int s)
+int rec(int a[],int size)
 {
-
-    if (a[j]==k && j<s)
+    if (a[j]==k && j<size)
         { j++;
           x++;
-          rec(a,s);
+          rec(a,size);
         }
-    else if(j<s)
+    else if(j<size)
     {
         j++;
-        rec(a,s);
+        rec(a,size);
     }
     return x;
 }
 
-main()
+int main()
 {
     printf("Enter size of array:");
-    scanf("%d",&s);
-
+    scanf("%d",&size);
+    
+    assert (size>0);    //Pre-condition : Array must not be empty.
+    
     printf("Enter the interger array:");
 
-    for(i=0;i<s;++i)
+    for(i=0;i<size;++i)
         scanf("%d",&a[i]);
 
     printf("Enter the nummber to check for frequency:");
     scanf("%d",&k);
 
-    occ = rec(a,s);
+    occ = rec(a,size);
+    
+    assert (occ>0);     //Post-condition : Frequency of an element must be non-negative.
 
     printf("Frequency of %d is %d",k,occ);
 
